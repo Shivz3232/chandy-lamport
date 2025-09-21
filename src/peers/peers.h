8c7b02ee825b8e93b194751e7da4995077ea710a
D G Shivu <dgshivu3232@gmail.com>
2025-09-21 19:10:54 -0400

    chore: segregate read & write peer connection params

--- src/peers/peers.h
+++ src/peers/peers.h
@@ -5,15 +5,19 @@
 #ifndef PEERS_H
 #define PEERS_H
 
-extern int numPeers;
-extern int connectedPeers;
+extern int numPeers; // NOTE: THIS NUMBER INCLUDES SELF
+extern int inboundConnections;
+extern int outboundConnections;
 
 struct Peer {
   int id;
   char* name;
-  int socket_fd;
-  struct addrinfo* addr_info;
-  struct addrinfo* chosen_addr_info;
+  int read_socket_fd;
+  struct addrinfo* read_addr_info;
+  struct addrinfo* read_chosen_addr_info;
+  int write_socket_fd;
+  struct addrinfo* write_addr_info;
+  struct addrinfo* write_chosen_addr_info;
   int connected;
 };
 
@@ -21,5 +25,6 @@ void* populatePeerInfo(struct Peer* peer);
 void freePeers(struct Peer* peers[]);
 struct Peer* getPredecessor(struct Peer* peers[]);
 struct Peer* getSuccessor(struct Peer* peers[]);
+void* dialPeers(void*);
 
 #endif
